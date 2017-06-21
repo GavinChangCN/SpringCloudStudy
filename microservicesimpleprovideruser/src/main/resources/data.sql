2e44248478d4c8827aa0ac995c345ec1bdbc13a2
章华隽 <gavinchangcn@163.com>
2017-06-21 09:15:49 +0800

    完成 Spring Cloud 的样本服务提供者和服务消费者

---
+++ microservicesimpleprovideruser/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into `user` (id, username, `password`, `name`, age, balance) values (1, 'account1', '12345', '张三', 20, 100.00);
+insert into `user` (id, username, `password`, `name`, age, balance) values (2, 'account2', '12345', '李四', 28, 180.00);
+insert into `user` (id, username, `password`, `name`, age, balance) values (3, 'account3', '12345', '王五', 32, 280.00);
