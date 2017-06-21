d4009c9f50defb1afd9f502a83c68df8fa39d516
章华隽 <gavinchangcn@163.com>
2017-06-21 09:22:16 +0800

    提交 Spring Cloud 的样本服务消费者

---
+++ microservice-simple-consumer-movie/src/main/java/com/walkin/cloud/study/user/entity/User.java
@@ -0,0 +1,67 @@
+package com.walkin.cloud.study.user.entity;
+
+import java.math.BigDecimal;
+
+/**
+ * User: Gavin
+ * E-mail: GavinChangCN@163.com
+ * Desc:
+ * Date: 2017-06-20
+ * Time: 17:06
+ */
+public class User {
+    private Long id;
+    private String username;
+    private String password;
+    private String name;
+    private Integer age;
+    private BigDecimal balance;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+}
