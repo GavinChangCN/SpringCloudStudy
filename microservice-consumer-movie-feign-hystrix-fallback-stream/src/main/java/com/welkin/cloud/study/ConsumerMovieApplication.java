435ba14c677ee25abe45575e320b31b3fecfc0e7
章华隽 <nefeed@163.com>
2017-08-18 23:50:12 +0800

    添加 Hystrix.Stream 和 Turbine.Stream 监控平台的Demo

--- microservice-consumer-movie-feign-hystrix-fallback-stream/src/main/java/com/welkin/cloud/study/ConsumerMovieApplication.java
+++ microservice-consumer-movie-feign-hystrix-fallback-stream/src/main/java/com/welkin/cloud/study/ConsumerMovieApplication.java
@@ -4,22 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableFeignClients
 @EnableCircuitBreaker
 public class ConsumerMovieApplication {
-
-	@Bean
-	@LoadBalanced
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
-	}
-
-	public static void main(String[] args) {
-		SpringApplication.run(ConsumerMovieApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(ConsumerMovieApplication.class, args);
+  }
 }
---
+++ microservice-consumer-movie-feign-hystrix-fallback-stream/src/main/java/com/welkin/cloud/study/user/controller/MovieController.java
@@ -0,0 +1,21 @@
+package com.welkin.cloud.study.user.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.welkin.cloud.study.user.entity.User;
+import com.welkin.cloud.study.user.feign.UserFeignClient;
+
+@RestController
+public class MovieController {
+  @Autowired
+  private UserFeignClient userFeignClient;
+
+  @GetMapping("/user/{id}")
+  public User findById(@PathVariable Long id) {
+    return this.userFeignClient.findById(id);
+  }
+}
