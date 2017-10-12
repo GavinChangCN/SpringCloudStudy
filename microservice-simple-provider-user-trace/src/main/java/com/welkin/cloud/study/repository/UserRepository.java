9ecb4894d310bfe87d74b23663f891ead163e752
章华隽 <nefeed@163.com>
2017-10-13 00:00:20 +0800

    添加 Spring Cloud Sleuth 的Demo

---
+++ microservice-simple-provider-user-trace/src/main/java/com/welkin/cloud/study/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.welkin.cloud.study.repository;
+
+import com.welkin.cloud.study.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * User: Gavin
+ * E-mail: GavinChangCN@163.com
+ * Desc:
+ * Date: 2017-06-20
+ * Time: 17:09
+ */
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
